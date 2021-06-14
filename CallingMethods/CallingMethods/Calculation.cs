825948ffded61aaf5a563ed7eef5e28375b37ac1
Mario Munoz <mario.zonum@gmail.com>
2021-06-14 16:31:56 -0400

    Create basic method calling

---
+++ CallingMethods/CallingMethods/Calculation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public class Calculation
+    {
+        public static int Addition(int num)
+        {
+           int addNum = num + 75;
+            return addNum;
+        }
+
+        public static int subtraction(int num)
+        {
+            int subNum = num - 12;
+            return subNum;
+        }
+
+        public static int division(int num)
+        {
+            int divNum = num / 2;
+            return divNum;
+        }
+
+    }
+}
--- CallingMethods/CallingMethods/Program.cs
+++ CallingMethods/CallingMethods/Program.cs
@@ -6,7 +6,12 @@ namespace CallingMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter a number you would like to do math with.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(userInput + " plus 75 is " + Calculation.Addition(userInput));
+            Console.WriteLine(userInput + " minus 12 is " + Calculation.subtraction(userInput));
+            Console.WriteLine(userInput + " divided by 2 is " + Calculation.division(userInput));
         }
     }
 }
