e2c543a433deced149b9897349e2c088faa36837
Mario Munoz <mario.zonum@gmail.com>
2021-06-14 10:14:03 -0400

    Create project files

---
+++ stringsAndIntegers/stringsAndIntegers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stringsAndIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
